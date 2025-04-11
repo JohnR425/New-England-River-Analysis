a5e4ad99d7829ac0398b8c3139fdcfb8382d19a7
natan <72627669+natkai2040@users.noreply.github.com>
2025-04-11 13:33:04 -0400

    Added Fetching of Gage Data

---
+++ modules/WorkerAPI.js
@@ -0,0 +1,20 @@
+async function getGageByState(stateName) { 
+    let url = `https://db-worker.nwai.workers.dev/gages?state=${stateName}`
+    let gageInfo = await fetch(url)
+        .then(res => res.json())
+        .catch((error) => {
+            console.log("error:", error);
+            return []
+        })
+    console.log("Fetched", gageInfo.length, "gages from Database with parameter", stateName)
+    return gageInfo
+}
+getGageByState("Maine")
+
+// fetch('https://db-worker.nwai.workers.dev/states').then(res => res.json()).then(console.log);
+
+// fetch('https://db-worker.nwai.workers.dev/gages?state=Massachusetts').then(res => res.json()).then(console.log);
+
+// fetch('https://db-worker.nwai.workers.dev/data?site=1010070&begin=2010-01-01&end=2010-01-07')
+//   .then(res => res.json())
+//   .then(console.log);
