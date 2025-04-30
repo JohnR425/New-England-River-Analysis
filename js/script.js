b5e9cb3d1911c94b9e1103a3b921385dc8808c84
JohnR425 <87833047+JohnR425@users.noreply.github.com>
2025-04-30 14:04:20 -0400

    Removed median data from table + Added gage summary component

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 //Setup Selection
 let selectedState = null;
+
 getStates().then(function (data) {
   selectedState = setupTableSelection(data);
   getGagesByState(selectedState).then(function (data) {
     //Setup Table
     setupTable(data);
+    //Setup Gage Summary
+    setupGageSummary(data[0]);
 
-    //Setup Line Chart
     getStatsByGageID(data[0].site_number, "2010-01-01", "2010-12-31")
     .then(function (data) {
         let discharges = data.map(elem => {return elem.mean_discharge});
         let precipitation = data.map(elem => {return elem.precipitation});
+        //Setup Line Chart
         setupLineChart(discharges, precipitation);
     });
   });
