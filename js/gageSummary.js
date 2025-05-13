56eb23d763f8321ba7b45b8a019b84113a5162d4
JohnR425 <87833047+JohnR425@users.noreply.github.com>
2025-05-12 21:57:55 -0400

    gage summary clean up

--- js/gageSummary.js
+++ js/gageSummary.js
@@ -1,11 +1,10 @@
+/**
+ * @param data = array of all gage point objects in the default selected state
+ */
 function setupGageSummary(data) {
     const textDiv = d3.select("#gage-summary-div");
-
-    console.log(Object.entries(data));
     let dataArr = Object.entries(data);
-    console.log("DATA ARR", dataArr);
 
-    //site_name
     textDiv.append("div")
         .classed("gage-summary", true)
         .classed("heading", true)
@@ -13,21 +12,22 @@ function setupGageSummary(data) {
         .style("font-weight", "bold")
         .text("Selected Gage Point: ");
 
+    //appending site name
     textDiv.append("div")
         .classed("gage-summary", true)
         .classed("heading", true)
         .classed("selected-gage-name", true)
         .text(dataArr[1][1]);
 
-    //site_number, elevation, lat, long.
+    //site_number, elevation, latitude, longitude
     const staticGageInfo = [
         dataArr[0][1],
         dataArr[3][1],
         dataArr[4][1],
         dataArr[5][1]
     ];
-    console.log("STATIC ARR", staticGageInfo);
 
+    //5th, 10th, median, 90th, 95th percentile discharge
     const dischargeInfo = [
         dataArr[6][1],
         dataArr[7][1],
@@ -35,82 +35,36 @@ function setupGageSummary(data) {
         dataArr[9][1],
         dataArr[10][1]
     ];
-    console.log("DISCHARGE ARR", dischargeInfo);
     
-    const bodyLine1 = textDiv.append("div")
+    //appending div for storing static gage info
+    const staticInfoDiv = textDiv.append("div")
                               .classed("gage-summary", true)
                               .classed("body", true)
                               .classed("gage-summary-basic-info", true);
 
-    const bodyLine2 = textDiv.append("div")
+    //appending div for storing dynamic gage info
+    const dynamicInfoDiv = textDiv.append("div")
                               .classed("gage-summary", true)
                               .classed("body", true)
                               .classed("gage-summary-median-info", true);
 
-    bodyLine1.selectAll("div")
-          .data(staticGageInfo)
-          .enter()
-          .append("div")
-          .attr("class", "gage-summary")
-          .html((d, i) => {
-              console.log(d);
-              if (i == 0) {
-                return "<strong>Site Number:</strong> <br>" + d;
-              }
-              else if (i == 1) {
-                return "<strong>Elevation (ft):</strong> <br>" + d;
-              }
-              else if (i == 2) {
-                return "<strong>Latitude: </strong> <br>" + d.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + " N";
-              }
-              else if (i == 3) {
-                return "<strong>Longitude: </strong> <br>" + d.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + " E";
-              }
-          });
-      
-    bodyLine2.selectAll("div")
-              .data(dischargeInfo)
-              .enter()
-              .append("div")
-              .attr("class", "gage-summary")
-              .html((d, i) => {
-                  console.log(d);
-                  if (i == 0) {
-                    return "<strong>5th Percentile Discharge: </strong> <br>" + d.toLocaleString('en-US', { minimumFractionDigits: 1, maximumFractionDigits: 1 }) + " ft<sup>3</sup> / sec";
-                  }
-                  else if (i == 1) {
-                    return "<strong>10th Percentile Discharge: </strong> <br>" + d.toLocaleString('en-US', { minimumFractionDigits: 1, maximumFractionDigits: 1 }) + " ft<sup>3</sup> / sec";
-                  }
-                  else if (i == 2) {
-                    return "<strong>Median Discharge: </strong> <br>" + d.toLocaleString('en-US', { minimumFractionDigits: 1, maximumFractionDigits: 1 }) + " ft<sup>3</sup> / sec";
-                  }
-                  else if (i == 3) {
-                    return "<strong>90th Percentile Discharge: </strong> <br>" + d.toLocaleString('en-US', { minimumFractionDigits: 1, maximumFractionDigits: 1 }) + " ft<sup>3</sup> / sec";
-                  }
-                  else if (i == 4) {
-                    return "<strong>95th Percentile Discharge: </strong> <br>" + d.toLocaleString('en-US', { minimumFractionDigits: 1, maximumFractionDigits: 1 }) + " ft<sup>3</sup> / sec";
-                  }
-              });                       
+    populateDivs(staticGageInfo, dischargeInfo);                      
 }
 
 function updateGageSummary(data) {
-  const textDiv = d3.select("#gage-summary");
-
     let dataArr = Object.entries(data);
-    console.log("DATA ARR", dataArr);
-
     d3.select(".selected-gage-name")
         .text(dataArr[1][1]);
 
-    //site_number, elevation, lat, long.
+    //site_number, elevation, latitude, longitude
     const staticGageInfo = [
         dataArr[0][1],
         dataArr[3][1],
         dataArr[4][1],
         dataArr[5][1]
     ];
-    console.log("STATIC ARR", staticGageInfo);
 
+    //5th, 10th, median, 90th, 95th percentile discharge
     const dischargeInfo = [
         dataArr[6][1],
         dataArr[7][1],
@@ -118,13 +72,18 @@ function updateGageSummary(data) {
         dataArr[9][1],
         dataArr[10][1]
     ];
-
-    console.log("DISCHARGE ARR", dischargeInfo);
+    populateDivs(staticGageInfo, dischargeInfo);
     
-    const bodyLine1 = d3.select(".gage-summary-basic-info");
-    const bodyLine2 = d3.select(".gage-summary-median-info");
+}
 
-    bodyLine1.selectAll("div")
+function populateDivs(staticGageInfo, dischargeInfo) {
+  //selecting div for storing static gage info
+  const staticInfoDiv = d3.select(".gage-summary-basic-info");
+  //selecting div for storing dynamic gage info
+  const dynamicInfoDiv = d3.select(".gage-summary-median-info");
+  
+  //populating static info div with text
+    staticInfoDiv.selectAll("div")
           .data(staticGageInfo)
           .join("div")
           .attr("class", "gage-summary")
@@ -144,7 +103,8 @@ function updateGageSummary(data) {
               }
           });
       
-    bodyLine2.selectAll("div")
+    //populating dynamic info div with text
+    dynamicInfoDiv.selectAll("div")
               .data(dischargeInfo)
               .join("div")
               .attr("class", "gage-summary")
